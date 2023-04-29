c5b628f7feb764b12e91fed38a59eb19b8edcef8
cqh <cqh963852@hotmail.com>
2023-04-29 13:59:27 +0800

    feat: style (#1)
    * chore: clean project
    
    * feat: add camphora-action
    
    * feat: add web-app

---
+++ web-app/vite.config.ts
@@ -0,0 +1,16 @@
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import path from "path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vanillaExtractPlugin()],
+  resolve: {
+    alias: {
+      "@idealjs/camphora-action": path.resolve(
+        __dirname,
+        "../packages/camphora-action/index.ts"
+      ),
+    },
+  },
+});
