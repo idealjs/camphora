78b56e3a4f59a0f02732be4c7ff17d567ecb9f01
cqh963852 <cqh963852@hotmail.com>
2023-04-29 13:51:22 +0800

    feat: add camphora-action

---
+++ packages/camphora-action/src/clickReaction.css.ts
@@ -0,0 +1,11 @@
+import { style } from "@vanilla-extract/css";
+
+export const clickReaction = style({});
+
+export const clickReactionInput = style({
+  selectors: {
+    [`${clickReaction} &`]: {
+      display: "none",
+    },
+  },
+});
