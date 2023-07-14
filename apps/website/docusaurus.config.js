193ce22f47cabb2e0ce18ebc9ddd3a1e66ec7b77
cqh963852 <cqh963852@hotmail.com>
2023-07-14 21:31:39 +0800

    chore: update url

--- apps/website/docusaurus.config.js
+++ apps/website/docusaurus.config.js
@@ -12,10 +12,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://camphora-ui.idealjs.com",
+  url: "https://idealjs.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/camphora",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
