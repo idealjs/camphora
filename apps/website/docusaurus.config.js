59541e7619352a701db25a1f1652dae6d75dc02d
cqh963852 <cqh963852@hotmail.com>
2023-07-14 21:43:23 +0800

    chore: update title

--- apps/website/docusaurus.config.js
+++ apps/website/docusaurus.config.js
@@ -7,8 +7,8 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Camphora",
-  tagline: "Camphora are reliable",
+  title: "Camphora-Styled",
+  tagline: "Camphora-Styled are reliable",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/introduction">
-            Docusaurus Tutorial - 5min ⏱️
+            Camphora-Styled Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
