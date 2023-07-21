c267c53fd88d61f4ee8fa33fc715846420999946
cqh963852 <cqh963852@hotmail.com>
2023-07-22 00:04:18 +0800

    feat: add popover

---
+++ packages/camphora-styled/src/popover.css.ts
@@ -0,0 +1,53 @@
+import { style } from "@vanilla-extract/css";
+
+import { clickReaction, clickReactionInput } from "./actions";
+
+export const popover = style({
+  position: "relative",
+});
+
+export const popoverContent = style({
+  visibility: "hidden",
+  position: "absolute",
+  selectors: {
+    [`${clickReaction} > ${clickReactionInput}:checked ~ &`]: {
+      visibility: "visible",
+    },
+  },
+});
+
+export const popoverLeft = style({
+  selectors: {
+    [`${popover} > &`]: {
+      left: "auto",
+      right: "100%",
+    },
+  },
+});
+
+export const popoverRight = style({
+  selectors: {
+    [`${popover} > &`]: {
+      left: "100%",
+      right: "auto",
+    },
+  },
+});
+
+export const popoverBottom = style({
+  selectors: {
+    [`${popover} > &`]: {
+      bottom: "auto",
+      top: "100%",
+    },
+  },
+});
+
+export const popoverTop = style({
+  selectors: {
+    [`${popover} > &`]: {
+      bottom: "100%",
+      top: "auto",
+    },
+  },
+});
