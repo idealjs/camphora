21dca4cbeb396baa790ee06d75e76d54dd8fe3a8
cqh963852 <cqh963852@hotmail.com>
2023-07-23 09:54:07 +0800

    feat: update popover direction

--- packages/camphora-styled/src/previewBlock.css.ts
+++ packages/camphora-styled/src/previewBlock.css.ts
@@ -6,5 +6,5 @@ export const previewBlock = style({
   padding: "16px",
   backgroundSize: "5px 5px",
   backgroundImage: `radial-gradient(${vars.colors.base.content} 0.5px ,${vars.colors.base[200]} 0.5px)`,
-  minHeight: "300px",
+  height: "300px",
 });
