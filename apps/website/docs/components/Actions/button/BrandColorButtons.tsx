91c9c0594aafbfd6a9467b4b5939c174ba2e2c07
wushide <1747583024@qq.com>
2023-07-31 23:43:09 +0800

    feat: add button components

--- apps/website/docs/components/Actions/button/BrandColorButtons.tsx
+++ apps/website/docs/components/Actions/button/BrandColorButtons.tsx
@@ -1,12 +1,50 @@
-import { button, buttonGhost } from "@idealjs/camphora-styled";
+import {
+  accentButton,
+  button,
+  buttonGhost,
+  errorButton,
+  infoButton,
+  neutralButton,
+  primaryButton,
+  secondaryButton,
+  successButton,
+  warningButton,
+} from "@idealjs/camphora-styled";
 import clsx from "clsx";
 import React from "react";
 
 const Button = () => {
+  const buttonSpacing = 8; // 设置按钮之间的垂直间距（以像素为单位）
+
   return (
     <div>
-      <div className={button}>Button</div>
-      <div className={clsx(button, buttonGhost)}>Button</div>
+      <div className={clsx(button, neutralButton)} style={{ marginRight: buttonSpacing }}>
+        neutral
+      </div>
+      <div className={clsx(button, buttonGhost)} style={{ marginRight: buttonSpacing }}>
+        Ghost
+      </div>
+      <div className={clsx(button, primaryButton)} style={{ marginRight: buttonSpacing }}>
+        primary
+      </div>
+      <div className={clsx(button, infoButton)} style={{ marginRight: buttonSpacing }}>
+        info
+      </div>
+      <div className={clsx(button, successButton)} style={{ marginRight: buttonSpacing }}>
+        success
+      </div>
+      <div className={clsx(button, warningButton)} style={{ marginRight: buttonSpacing }}>
+        warning
+      </div>
+      <div className={clsx(button, errorButton)} style={{ marginRight: buttonSpacing }}>
+        error
+      </div>
+      <div className={clsx(button, secondaryButton)} style={{ marginRight: buttonSpacing }}>
+        secondary
+      </div>
+      <div className={clsx(button, accentButton)} style={{ marginRight: buttonSpacing }}>
+      accent
+      </div>
     </div>
   );
 };
--- packages/camphora-styled/src/button.css.ts
+++ packages/camphora-styled/src/button.css.ts
@@ -2,7 +2,7 @@ import { style } from "@vanilla-extract/css";
 
 import { vars } from "./theme.css";
 
-export const button = style({
+export const button: Object = style({
   cursor: "pointer",
   display: "inline-flex",
   flexShrink: 0,
@@ -12,17 +12,75 @@ export const button = style({
   height: "48px",
   minHeight: "48px",
   padding: "0px 16px 0px 16px",
-  backgroundColor: vars.colors.neutral.bg,
-  color: vars.colors.neutral.content,
+  backgroundColor: vars.colors.primary.bg, // Use primary background color
+  color: vars.colors.primary.content, // Use primary content color
+  borderRadius: vars.rounded.btn, // Apply rounded corners from theme
+  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)", // Add a subtle box shadow
+  transition: "background-color 0.2s ease-in-out", // Add transition effect
+  ":hover": {
+    backgroundColor: vars.colors.primary.focus, // Use primary focus color on hover
+  },
+  ":active": {
+    transform: "scale(0.95)", // Add a slight scaling effect on click
+  },
 });
 
 export const buttonActive = style({});
 
+export const primaryButton = style({
+  ...button,
+  backgroundColor: vars.colors.primary.bg,
+  color: vars.colors.primary.content,
+});
+
+export const infoButton = style({
+  ...button,
+  backgroundColor: vars.colors.info.bg,
+  color: vars.colors.info.content,
+});
+
+export const successButton = style({
+  ...button,
+  backgroundColor: vars.colors.success.bg,
+  color: vars.colors.success.content,
+});
+
+export const warningButton = style({
+  ...button,
+  backgroundColor: vars.colors.warning.bg,
+  color: vars.colors.warning.content,
+});
+
+export const errorButton = style({
+  ...button,
+  backgroundColor: vars.colors.error.bg,
+  color: vars.colors.error.content,
+});
+
+export const neutralButton = style({
+  ...button,
+  backgroundColor: vars.colors.neutral.bg,
+  color: vars.colors.neutral.content,
+});
+
+export const secondaryButton = style({
+  ...button,
+  backgroundColor: vars.colors.secondary.bg,
+  color: vars.colors.secondary.content,
+});
+
+export const accentButton = style({
+  ...button,
+  backgroundColor: vars.colors.accent.bg,
+  color: vars.colors.accent.content,
+});
+
 export const buttonGhost = style({
+  ...button,
   backgroundColor: "transparent",
   color: "currentcolor",
   selectors: {
-    [`&:hover, ${buttonActive}&`]: {
+    "&:hover": {
       backgroundColor: `color-mix(in srgb, ${vars.colors.base.content} ${vars.opacity.btn}, #fff0)`,
     },
   },
