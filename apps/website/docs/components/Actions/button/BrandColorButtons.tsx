8fabfb707322346e26f6d65705599ba61a202011
wushide <44914677+wushide@users.noreply.github.com>
2023-08-03 16:50:45 +0800

    feat: add button components (#20)
    * feat: add button components
    
    * fix: review problems
    
    * fix: review problems
    
    * feat: add button components
    
    * fix: review problems
    
    * fix: review problems
    
    * fix: modified priview issues
    
    * fix: modified priview issues
    
    * fix: modified priview issues
    
    * feat: show button components code
    
    * feat: show button components code
    
    * fix: line endings
    
    * fix: Normalize line breaks to LF

--- apps/website/docs/components/Actions/button/BrandColorButtons.tsx
+++ apps/website/docs/components/Actions/button/BrandColorButtons.tsx
@@ -1,14 +1,40 @@
-import { button, buttonGhost } from "@idealjs/camphora-styled";
+import {
+  button,
+  buttonAccent,
+  buttonGhost,
+  buttonNeutral,
+  buttonPrimary,
+  buttonSecondary,
+} from "@idealjs/camphora-styled";
 import clsx from "clsx";
-import React from "react";
+import React, { type CSSProperties } from "react";
 
+const buttonSpacing = 4; // 设置按钮之间的垂直间距（以像素为单位）
+const style: CSSProperties = {
+  margin: buttonSpacing
+}
 const Button = () => {
+
   return (
     <div>
-      <div className={button}>Button</div>
-      <div className={clsx(button, buttonGhost)}>Button</div>
+      <div className={clsx(button, buttonNeutral)} style={style}>
+        neutral
+      </div>
+      <div className={clsx(button, buttonGhost)} style={style}>
+        Ghost
+      </div>
+      <div className={clsx(button, buttonPrimary)} style={style}>
+        primary
+      </div>
+      <div className={clsx(button, buttonSecondary)} style={style}>
+        secondary
+      </div>
+      <div className={clsx(button, buttonAccent)} style={style}>
+        accent
+      </div>
     </div>
   );
 };
 
 export default Button;
+
--- packages/camphora-styled/src/button.css.ts
+++ packages/camphora-styled/src/button.css.ts
@@ -2,6 +2,18 @@ import { style } from "@vanilla-extract/css";
 
 import { vars } from "./theme.css";
 
+export const buttonActive = style({});
+
+export const buttonPrimary = style({});
+
+export const buttonNeutral = style({});
+
+export const buttonSecondary = style({});
+
+export const buttonAccent = style({});
+
+export const buttonGhost = style({});
+
 export const button = style({
   cursor: "pointer",
   display: "inline-flex",
@@ -12,18 +24,57 @@ export const button = style({
   height: "48px",
   minHeight: "48px",
   padding: "0px 16px 0px 16px",
-  backgroundColor: vars.colors.neutral.bg,
-  color: vars.colors.neutral.content,
-});
-
-export const buttonActive = style({});
-
-export const buttonGhost = style({
-  backgroundColor: "transparent",
-  color: "currentcolor",
+  borderRadius: vars.rounded.btn, // Apply rounded corners from theme
+  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)", // Add a subtle box shadow
+  transition: "background-color 0.2s ease-in-out", // Add transition effect
   selectors: {
-    [`&:hover, ${buttonActive}&`]: {
+    [`&:active, &${buttonActive}`]: {
+      transform: "scale(0.95)", // Add a slight scaling effect on click
+    },
+
+    // primary
+    [`&${buttonPrimary}`]: {
+      backgroundColor: vars.colors.primary.bg,
+      color: vars.colors.primary.content,
+    },
+    [`&${buttonPrimary}:hover`]: {
+      backgroundColor: vars.colors.primary.focus,
+    },
+
+    // neutral
+    [`&${buttonNeutral}`]: {
+      backgroundColor: vars.colors.neutral.bg,
+      color: vars.colors.neutral.content,
+    },
+    [`&${buttonNeutral}:hover`]: {
+      backgroundColor: vars.colors.neutral.focus,
+    },
+    
+    // secondary
+    [`&${buttonSecondary}`]: {
+      backgroundColor: vars.colors.secondary.bg,
+      color: vars.colors.secondary.content,
+    },
+    [`&${buttonSecondary}:hover`]: {
+      backgroundColor: vars.colors.secondary.focus,
+    },
+    
+    // accent
+    [`&${buttonAccent}`]: {
+      backgroundColor: vars.colors.accent.bg,
+      color: vars.colors.accent.content,
+    },
+    [`&${buttonAccent}:hover`]: {
+      backgroundColor: vars.colors.accent.focus,
+    },
+
+    // ghost
+    [`&${buttonGhost}`]: {
+      backgroundColor: "transparent",
+      color: "currentcolor",
+    },
+    [`&${buttonGhost}:hover`]: {
       backgroundColor: `color-mix(in srgb, ${vars.colors.base.content} ${vars.opacity.btn}, #fff0)`,
     },
-  },
+  }
 });
