7dceb58a495b21627f4803756dba8fc2aaf9d572
wushide <1747583024@qq.com>
2023-08-03 15:02:08 +0800

    fix: modified priview issues

--- apps/website/docs/components/Actions/button/BrandColorButtons.tsx
+++ apps/website/docs/components/Actions/button/BrandColorButtons.tsx
@@ -1,23 +1,10 @@
 import {
-<<<<<<< HEAD
   button,
   buttonAccent,
   buttonGhost,
   buttonNeutral,
   buttonPrimary,
   buttonSecondary,
-=======
-  accentButton,
-  button,
-  buttonGhost,
-  errorButton,
-  infoButton,
-  neutralButton,
-  primaryButton,
-  secondaryButton,
-  successButton,
-  warningButton,
->>>>>>> 91c9c05 (feat: add button components)
 } from "@idealjs/camphora-styled";
 import clsx from "clsx";
 import React, { type CSSProperties } from "react";
@@ -27,7 +14,6 @@ const style: CSSProperties = {
   margin: buttonSpacing
 }
 const Button = () => {
-<<<<<<< HEAD
 
   return (
     <div>
@@ -45,38 +31,6 @@ const Button = () => {
       </div>
       <div className={clsx(button, buttonAccent)} style={style}>
         accent
-=======
-  const buttonSpacing = 8; // 设置按钮之间的垂直间距（以像素为单位）
-
-  return (
-    <div>
-      <div className={clsx(button, neutralButton)} style={{ marginRight: buttonSpacing }}>
-        neutral
-      </div>
-      <div className={clsx(button, buttonGhost)} style={{ marginRight: buttonSpacing }}>
-        Ghost
-      </div>
-      <div className={clsx(button, primaryButton)} style={{ marginRight: buttonSpacing }}>
-        primary
-      </div>
-      <div className={clsx(button, infoButton)} style={{ marginRight: buttonSpacing }}>
-        info
-      </div>
-      <div className={clsx(button, successButton)} style={{ marginRight: buttonSpacing }}>
-        success
-      </div>
-      <div className={clsx(button, warningButton)} style={{ marginRight: buttonSpacing }}>
-        warning
-      </div>
-      <div className={clsx(button, errorButton)} style={{ marginRight: buttonSpacing }}>
-        error
-      </div>
-      <div className={clsx(button, secondaryButton)} style={{ marginRight: buttonSpacing }}>
-        secondary
-      </div>
-      <div className={clsx(button, accentButton)} style={{ marginRight: buttonSpacing }}>
-      accent
->>>>>>> 91c9c05 (feat: add button components)
       </div>
     </div>
   );
