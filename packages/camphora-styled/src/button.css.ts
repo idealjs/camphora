bd802aa8f9bebc13711076f7ce6d3bce49d7a6b1
wushide <1747583024@qq.com>
2023-08-03 11:05:54 +0800

    fix: review problems

--- packages/camphora-styled/src/button.css.ts
+++ packages/camphora-styled/src/button.css.ts
@@ -2,7 +2,19 @@ import { style } from "@vanilla-extract/css";
 
 import { vars } from "./theme.css";
 
-export const button: Object = style({
+export const buttonActive = style({});
+
+export const buttonPrimary = style({});
+
+export const buttonNeutral = style({});
+
+export const buttonSecondary = style({});
+
+export const buttonAccent = style({});
+
+export const buttonGhost = style({});
+
+export const button = style({
   cursor: "pointer",
   display: "inline-flex",
   flexShrink: 0,
@@ -12,76 +24,57 @@ export const button: Object = style({
   height: "48px",
   minHeight: "48px",
   padding: "0px 16px 0px 16px",
-  backgroundColor: vars.colors.primary.bg, // Use primary background color
-  color: vars.colors.primary.content, // Use primary content color
   borderRadius: vars.rounded.btn, // Apply rounded corners from theme
   boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)", // Add a subtle box shadow
   transition: "background-color 0.2s ease-in-out", // Add transition effect
-  ":hover": {
-    backgroundColor: vars.colors.primary.focus, // Use primary focus color on hover
-  },
-  ":active": {
-    transform: "scale(0.95)", // Add a slight scaling effect on click
-  },
-});
-
-export const buttonActive = style({});
-
-export const primaryButton = style({
-  ...button,
-  backgroundColor: vars.colors.primary.bg,
-  color: vars.colors.primary.content,
-});
-
-export const infoButton = style({
-  ...button,
-  backgroundColor: vars.colors.info.bg,
-  color: vars.colors.info.content,
-});
-
-export const successButton = style({
-  ...button,
-  backgroundColor: vars.colors.success.bg,
-  color: vars.colors.success.content,
-});
-
-export const warningButton = style({
-  ...button,
-  backgroundColor: vars.colors.warning.bg,
-  color: vars.colors.warning.content,
-});
-
-export const errorButton = style({
-  ...button,
-  backgroundColor: vars.colors.error.bg,
-  color: vars.colors.error.content,
-});
-
-export const neutralButton = style({
-  ...button,
-  backgroundColor: vars.colors.neutral.bg,
-  color: vars.colors.neutral.content,
-});
-
-export const secondaryButton = style({
-  ...button,
-  backgroundColor: vars.colors.secondary.bg,
-  color: vars.colors.secondary.content,
-});
-
-export const accentButton = style({
-  ...button,
-  backgroundColor: vars.colors.accent.bg,
-  color: vars.colors.accent.content,
-});
-
-export const buttonGhost = style({
-  ...button,
-  backgroundColor: "transparent",
-  color: "currentcolor",
   selectors: {
-    "&:hover": {
+    [`&:active, &${buttonActive}`]: {
+      transform: "scale(0.95)", // Add a slight scaling effect on click
+    },
+
+    // primary
+    [`&${buttonPrimary}`]: {
+      backgroundColor: vars.colors.primary.bg,
+      color: vars.colors.primary.content,
+    },
+    [`&${buttonPrimary}:hover`]: {
+      backgroundColor: vars.colors.primary.focus,
+    },
+
+    // neutral
+    [`&${buttonNeutral}`]: {
+      backgroundColor: vars.colors.neutral.bg,
+      color: vars.colors.neutral.content,
+    },
+    [`&${buttonNeutral}:hover`]: {
+      backgroundColor: vars.colors.neutral.focus,
+    },
+    
+    // secondary
+    [`&${buttonSecondary}`]: {
+      backgroundColor: vars.colors.secondary.bg,
+      color: vars.colors.secondary.content,
+    },
+    [`&${buttonSecondary}:hover`]: {
+      backgroundColor: vars.colors.secondary.focus,
+    },
+    
+    // accent
+    [`&${buttonAccent}`]: {
+      backgroundColor: vars.colors.accent.bg,
+      color: vars.colors.accent.content,
+    },
+    [`&${buttonAccent}:hover`]: {
+      backgroundColor: vars.colors.accent.focus,
+    },
+
+    // ghost
+    [`&${buttonGhost}`]: {
+      backgroundColor: "transparent",
+      color: "currentcolor",
+    },
+    [`&${buttonGhost}:hover`]: {
       backgroundColor: `color-mix(in srgb, ${vars.colors.base.content} ${vars.opacity.btn}, #fff0)`,
     },
-  },
+  }
 });
