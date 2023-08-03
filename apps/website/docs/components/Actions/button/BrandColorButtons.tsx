7a954e26a15f9705c5f4491578c97d065441ef83
wushide <1747583024@qq.com>
2023-08-03 14:55:28 +0800

    fix: modified priview issues

--- apps/website/docs/components/Actions/button/BrandColorButtons.tsx
+++ apps/website/docs/components/Actions/button/BrandColorButtons.tsx
@@ -7,27 +7,30 @@ import {
   buttonSecondary,
 } from "@idealjs/camphora-styled";
 import clsx from "clsx";
-import React from "react";
+import React, { type CSSProperties } from "react";
 
-const buttonSpacing = 8; // 设置按钮之间的垂直间距（以像素为单位）
+const buttonSpacing = 4; // 设置按钮之间的垂直间距（以像素为单位）
+const style: CSSProperties = {
+  margin: buttonSpacing
+}
 const Button = () => {
 
   return (
     <div>
-      <div className={clsx(button, buttonNeutral)} style={{ marginRight: buttonSpacing }}>
+      <div className={clsx(button, buttonNeutral)} style={style}>
         neutral
       </div>
-      <div className={clsx(button, buttonGhost)} style={{ marginRight: buttonSpacing }}>
+      <div className={clsx(button, buttonGhost)} style={style}>
         Ghost
       </div>
-      <div className={clsx(button, buttonPrimary)} style={{ marginRight: buttonSpacing }}>
+      <div className={clsx(button, buttonPrimary)} style={style}>
         primary
       </div>
-      <div className={clsx(button, buttonSecondary)} style={{ marginRight: buttonSpacing }}>
+      <div className={clsx(button, buttonSecondary)} style={style}>
         secondary
       </div>
-      <div className={clsx(button, buttonAccent)} style={{ marginRight: buttonSpacing }}>
-      accent
+      <div className={clsx(button, buttonAccent)} style={style}>
+        accent
       </div>
     </div>
   );
