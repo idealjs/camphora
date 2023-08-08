b2ddc0d298cfb07d87ee70188f4667b0abef6321
wushide <1747583024@qq.com>
2023-08-08 09:38:03 +0800

    fix: optimize styles

--- apps/website/docs/components/Actions/popover/Bottom.tsx
+++ apps/website/docs/components/Actions/popover/Bottom.tsx
@@ -12,13 +12,6 @@ import React from "react";
 
 const id = "popoverBottom";
 
-const contentStyle = {
-  background: "#ffffff", // 添加背景颜色
-  boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)", // 添加阴影效果
-  padding: "10px", // 添加内边距
-  borderRadius: "5px", // 添加圆角
-}
-
 const Popover = () => {
   return (
     <div
@@ -37,7 +30,7 @@ const Popover = () => {
           <input id={id} type="checkbox" className={clickReactionInput} />
           open
         </label>
-        <div className={clsx(popoverContent, popoverBottom)} style={contentStyle}>hello popover</div>
+        <div className={clsx(popoverContent, popoverBottom)}>hello popover</div>
       </div>
     </div>
   );
