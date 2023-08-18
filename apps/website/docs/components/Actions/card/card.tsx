b6fbb3d50419ca76a7791de72f7635aa09137691
wushide <1747583024@qq.com>
2023-08-18 17:36:11 +0800

    feat: add card component

---
+++ apps/website/docs/components/Actions/card/card.tsx
@@ -0,0 +1,34 @@
+import {
+  button,
+  buttonPrimary,
+  card,
+  cardContent,
+  cardTitle,
+} from "@idealjs/camphora-styled";
+import clsx from "clsx";
+import React, { type CSSProperties } from "react";
+
+const cardStyle: CSSProperties = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+}
+
+const otherStyle: CSSProperties = {
+  width: 300
+}
+
+const Card = () => {
+  return (
+    <div style={cardStyle}>
+      <div className={clsx(card)} style={otherStyle}>
+        <div className={clsx(cardTitle)}>cardTitle!</div>
+        <div className={clsx(cardContent)}>If a dog chews shoes whose shoes does he choose?</div>
+        <button className={clsx(button, buttonPrimary)}>button</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
+
---
+++ packages/camphora-styled/src/card.css.ts
@@ -0,0 +1,38 @@
+import { style } from "@vanilla-extract/css";
+
+export const card = style({
+  backgroundColor: "#ffffff",
+  borderRadius: "8px",
+  boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+  padding: "16px",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  fontSize: "1rem",
+  position: "relative",
+  selectors: {
+    "&:hover": {
+      boxShadow: "0 8px 12px rgba(0, 0, 0, 0.2)",
+    },
+  },
+});
+
+export const cardTitle = style({
+  fontSize: "1.25rem",
+  fontWeight: "bold",
+  marginBottom: "8px",
+});
+
+export const cardContent = style({
+  marginBottom: "16px",
+  textAlign: "center",
+});
+
+export const cardImage = style({
+  width: "100%",
+  maxHeight: "200px",
+  objectFit: "cover",
+  borderRadius: "8px",
+  marginBottom: "16px",
+});
